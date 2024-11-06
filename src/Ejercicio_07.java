d18554c188a64fe316a5461322ea8467bcab6782
Franco <juafracas@alu.edu.gva.es>
2024-11-06 19:52:59 +0100

    hoy

--- src/Ejercicio_07.java
+++ src/Ejercicio_07.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Ejercicio_07 {
 
-    final static Scanner scan = new Scanner(System.in);
+    static final Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
 
@@ -10,18 +10,22 @@ public class Ejercicio_07 {
         do {
             op = menu();
 
-            switch (op){
+            switch (op) {
                 case 1 -> { //Multiplicar
                     System.out.println("Tabla de Multiplicar");
-                    System.out.print("Introduce el numero a multiplicar: ");
                     int num = dameUnNumero();
                     tablaMultiplicar(num);
                 }
                 case 2 -> { //Saludar
-                    System.out.println("Introduce un numero: ");
                     int rep = dameUnNumero();
                     String nombre = dameTexto();
                     saludar(nombre, rep);
+                }
+                case 3 -> { //Numero Primo
+
+                    int num = dameUnNumero();
+                    mostarPrimos(num);
+
                 }
             }
 
@@ -32,6 +36,7 @@ public class Ejercicio_07 {
 
     /**
      * Esta funcion devuelve un menu en bucle
+     *
      * @return op
      */
     public static int menu() {
@@ -46,17 +51,19 @@ public class Ejercicio_07 {
                     "5. Multiplicar n enteros\n6. Potencia\n7. Piramide\n8. Fibonacci\n9. Salir");
             op = scan.nextInt();
 
-        } while ( op < 1 || op > 9);
+        } while (op < 1 || op > 9);
         return op;
     }
 
 
     /**
      * Esta funcion devuelve un valor valido
+     *
      * @return numero
      */
-    public static int dameUnNumero(){
-        while (!scan.hasNextInt()){
+    public static int dameUnNumero() {
+        System.out.println("Introduce un numero: ");
+        while (!scan.hasNextInt()) {
             System.out.println("Entrada invalida. Porfavor ingresa un numero valido: ");
             scan.nextInt();
         }
@@ -68,21 +75,24 @@ public class Ejercicio_07 {
 
     /**
      * Esta funcion pide texto
+     *
      * @return nombre
      */
-    public static String dameTexto(){
+    public static String dameTexto() {
         System.out.println("Introduce el nombre: ");
         String nombre = scan.nextLine();
+
         return nombre;
     }
 
 
     /**
      * Esta funcion no devuelve nada, pero muestra una tabla de multiplicar
+     *
      * @param numero
      */
-    public static void tablaMultiplicar(int numero){
-        for (int i = 1; i <= 10; i++){
+    public static void tablaMultiplicar(int numero) {
+        for (int i = 1; i <= 10; i++) {
             System.out.println(i + " X " + numero + " = " + (i * numero));
         }
     }
@@ -90,14 +100,50 @@ public class Ejercicio_07 {
 
     /**
      * Funcion para repetir saludos
+     *
      * @param nombre
      * @param repeteciones
      */
-    public static void saludar(String nombre, int repeteciones){
+    public static void saludar(String nombre, int repeteciones) {
 
-        for (int i = 1; i <= repeteciones; i++){
+        for (int i = 1; i <= repeteciones; i++) {
             System.out.println("Hola " + nombre);
         }
 
+    }
+
+
+
+    public static boolean numeroPrimo(int numero) {
+        boolean esPrimo = true;
+
+        if (numero < 1) {
+            esPrimo = false;
+        } else {
+            for (int i = 2; i <= numero / 2; i++) {
+                if (numero % i == 0) {
+                    esPrimo = false;
+                }
+            }
+        }
+        return esPrimo;
+    }
+
+
+
+    public static void mostarPrimos(int cantidad){
+        int contador = 0;
+        int numero = 2;
+
+        System.out.println("Primeros " + cantidad + " numeros primos: ");
+
+        while(contador < cantidad){
+            if (numeroPrimo(numero)){
+                System.out.print(numero + " ");
+                contador++;
+            }
+                numero++;
+        }
+        System.out.println(" ");
     }
 }
